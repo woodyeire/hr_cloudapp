994da8a552080272955f8607fe52c03f643bcb42
woodyeire <johnmurphyty@gmail.com>
2015-07-31 19:14:29 +0000

    added standard users, with admin ability to create from dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :employees, :users
+
+  
   
   root to: "employees#index"
   
